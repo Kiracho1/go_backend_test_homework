fa53dbd9eae5afc0e52060e030356685a2c9f6af
Kiracho <acholov@bk.ru>
2026-09-12 15:24:00 +0300

    Average duration validation

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
-func Add(a, b int) int {
-	return a + b
+func average(values []int) (float64, error) {
+	if len(values) == 0 {
+		return 0, errors.New("невозможно вычислить среднее для пустого списка")
+	}
+	sum := 0
+	for _, v := range values {
+		sum += v
+	}
+	return float64(sum) / float64(len(values)), nil
 }
 
 func main() {
-	fmt.Println("Я домашка") // да-да, ты домашка
+	durations := []int{
+		120, 95, 110, 140, 85,
+	}
+	avg, err := average(durations)
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+	} else {
+		fmt.Printf("Среднее время обработки: %.2f мс\n", avg)
+	}
 }
